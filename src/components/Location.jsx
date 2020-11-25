355ff5be7d28d745854e63217300d36257d5cdb8
Joshua-Salvador <424joshuae.b.s@gmail.com>
2020-11-25 19:28:22 +0800

    Count update and Full Capacity Modal

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -4,10 +4,32 @@ import './Location.css'
 import {Card, Button, Modal} from 'react-bootstrap'
 
 function Location(props) {
+  // Modal
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleShow = () => {
+    if(count === 30) {
+      console.log(`Cannot enter, ${props.name} has reached maximum number of people.`);
+      return
+    } else {
+      setShow(true)
+    }
+    
+  };
+
+  // Count
+  const [count, setCount] = useState(29)
+
+  const addCountHandler = () => {
+    if(count === 30) {
+      console.log(`Cannot enter, ${props.name} has reached maximum number of people.`);
+      return
+    } else {
+      handleClose();
+      setCount(count + 1);
+    }
+  }
 
 
   return (
@@ -17,9 +39,13 @@ function Location(props) {
         <Card.Body>
           <Card.Title>{props.name}</Card.Title>
           <Card.Text>{props.address}</Card.Text>
+          <Card.Text>Capacity: {props.capacity}</Card.Text>
+          <Card.Text>Count: {count}</Card.Text>
           <Button variant="primary" onClick={handleShow}>Request Enter</Button>
         </Card.Body>
       </Card>
+
+      {/* Confirm Request Modal */}
       <Modal show={show} onHide={handleClose} centered>
         <Modal.Header closeButton>
           <Modal.Title>{props.name}</Modal.Title>
@@ -29,12 +55,20 @@ function Location(props) {
           <Button variant="warning" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={addCountHandler}>
             Save the World
           </Button>
         </Modal.Footer>
       </Modal>
-    
+
+      {/* Full Capacity Reached Modal */}
+      {/* <Modal>
+        <Modal.Header closeButton>
+          <Modal.Title>Full Capacity Reached.</Modal.Title>
+          <Modal.Body>Sorry, but {props.name} has reached full capacity.</Modal.Body>
+        </Modal.Header>
+      </Modal>
+     */}
     </div>
   )
 }
--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -1,41 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Locations.css"
 import Location from "./Location"
 
 //Bootstrap Components
 import {Container, Row, Col} from 'react-bootstrap';
 
+
 function Locations() {
-  const locations = [
+  const [locations, setLocations] = useState([
     {
     name: "McDonalds",
     address: "IT Park, Lahug, Cebu City", 
     capacity: 30,
-    count: 15,
+    count: 0,
     id: 0
     },
     {
     name: "Jollibee",
     address: "Ayala, Cebu City", 
     capacity: 30,
-    count: 15,
+    count: 0,
     id: 1
     },
     {
     name: "Pizza Hut",
     address: "Sm Seaside, SRP, Cebu City", 
     capacity: 30,
-    count: 15,
+    count: 0,
     id: 2
     },
     {
     name: "138 Mall",
     address: "Colon, Cebu City", 
     capacity: 30,
-    count: 15,
+    count: 0,
     id: 3
     },
-  ]
+  ])
+
+  // countHandler = (id) => {
+  //   setLocations([...locations, ])
+  // }
 
   return (
     <div className="container">
@@ -43,7 +48,7 @@ function Locations() {
         <Row>
             {locations.map((location) => 
               <Col key={location.id}>
-                <Location key={location.id} name={location.name} address={location.address} />
+                <Location key={location.id} name={location.name} address={location.address} capacity={location.capacity} count={location.count}/>
               </Col>
             )}
         </Row>
